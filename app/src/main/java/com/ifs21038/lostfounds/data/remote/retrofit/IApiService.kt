b4d80a5e153f70705ac347787652dd4942adae69
Jayfline Hutagalung <jayfline7@gmail.com>
2024-04-28 17:54:56 +0700

    Progress week 12 lagi1

--- app/src/main/java/com/ifs21038/lostfounds/data/remote/retrofit/IApiService.kt
+++ app/src/main/java/com/ifs21038/lostfounds/data/remote/retrofit/IApiService.kt
@@ -6,10 +6,13 @@ import com.ifs18005.delcomtodo.data.remote.response.DelcomLostFoundResponse
 import com.ifs18005.delcomtodo.data.remote.response.DelcomLostFoundsResponse
 import com.ifs18005.delcomtodo.data.remote.response.DelcomResponse
 import com.ifs18005.delcomtodo.data.remote.response.DelcomUserResponse
+import okhttp3.MultipartBody
 import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.Part
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -67,5 +70,12 @@ interface IApiService {
     @DELETE("lost-founds/{id}")
     suspend fun deleteLostFound(
         @Path("id") lostfoundId: Int
+    ): DelcomResponse
+
+    @Multipart
+    @POST("todos/{id}/cover")
+    suspend fun addCoverTodo(
+        @Path("id") todoId: Int,
+        @Part cover: MultipartBody.Part,
     ): DelcomResponse
 }
